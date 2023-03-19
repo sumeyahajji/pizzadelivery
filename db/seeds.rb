a853f7b4a13b89771c9f117824c717adaf1100ef
+ <+>
2023-03-20 00:07:26 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurant1 = Restaurant.create(name: "Pizzeria Uno", address: "123 Main St")
+restaurant2 = Restaurant.create(name: "Pizza Hut", address: "456 Oak St")
+pizza1 = Pizza.create(name: "Pepperoni", ingredient: "pepperoni")
+pizza2 = Pizza.create(name: "Margarita", ingredient: "tomatoes and basil")
+restaurant1.restaurant_pizzas.create(pizza: pizza1, price: 10)
+restaurant1.restaurant_pizzas.create(pizza: pizza2, price: 15)
+restaurant2.restaurant_pizzas.create(pizza: pizza1, price: 20)
